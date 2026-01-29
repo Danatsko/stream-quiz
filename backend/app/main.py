959256c7d281df460b95bea63c48f84b7f14c05a
Danatsko <oleksandrdanatsko@gmail.com>
2026-01-29 21:13:47 +0200

    feat(backend): add limiter
    - Create Limiter instance
    - Add SlowAPIMiddleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,8 +3,12 @@ from typing import AsyncGenerator, Any
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware
 
 from app.core.config import settings
+from app.core.limiter import limiter
 
 
 @asynccontextmanager
@@ -13,6 +17,12 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, Any]:
 
 
 app = FastAPI(lifespan=lifespan)
+app.state.limiter = limiter
+app.add_exception_handler(
+    RateLimitExceeded,
+    _rate_limit_exceeded_handler,
+)
+app.add_middleware(SlowAPIMiddleware)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.cors.origins,
