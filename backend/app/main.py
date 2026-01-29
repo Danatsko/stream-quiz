c33fc1c1b1b1ba451fca614417da0d60f0c99003
Danatsko <oleksandrdanatsko@gmail.com>
2026-01-29 20:58:14 +0200

    feat(backend): add CORSMiddleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,9 @@ from contextlib import asynccontextmanager
 from typing import AsyncGenerator, Any
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.core.config import settings
 
 
 @asynccontextmanager
@@ -10,3 +13,10 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, Any]:
 
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.cors.origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
