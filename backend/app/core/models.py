b8ccfc8ef8ee998a8e5c171b7a3bb78d304e6ff3
Danatsko <oleksandrdanatsko@gmail.com>
2026-01-30 17:16:30 +0200

    feat(backend): add base for database models
    - Add base for database models: Base, UUIDMixin, SoftDeleteMixin

---
+++ backend/app/core/models.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+import uuid
+
+from sqlalchemy import BigInteger, Uuid, DateTime, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    id: Mapped[int] = mapped_column(
+        BigInteger(),
+        primary_key=True,
+        autoincrement=True,
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
+
+
+class UUIDMixin:
+    uuid: Mapped[uuid.UUID] = mapped_column(
+        Uuid(),
+        unique=True,
+        nullable=False,
+        server_default=func.uuidv7(),
+    )
+
+
+class SoftDeleteMixin:
+    deleted_at: Mapped[datetime | None] = mapped_column(
+        DateTime(timezone=True),
+        nullable=True,
+    )
